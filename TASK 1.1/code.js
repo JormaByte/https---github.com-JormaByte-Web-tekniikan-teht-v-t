1fb7084a67836d0f2e3452deabd3add9c3e8dcb2
Jorma <joel.oksanen1223@gmail.com>
2022-12-16 13:41:38 +0200

    TASK 1.1

---
+++ TASK 1.1/code.js
@@ -0,0 +1,16 @@
+
+ var nappi = document.quiz.nappi.value ;
+
+function piilota() {
+
+
+
+    if (nappi=="piilota") {
+
+        alert("moro nääs") ;
+    } else {
+
+        alert("Mee pois!") ;
+    }
+
+}
